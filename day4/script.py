417095e1639772ec52595e40be9ba34c8363a569
Mauzerov <mauzer.official.1@gmail.com>
2023-12-04 18:01:38 +0100

    reformat code nicely

--- day4/script.py
+++ day4/script.py
@@ -3,34 +3,32 @@ from pprint import pprint
 print(__file__)
 
 
-with open('input.txt', 'r') as f:
+with open('day4/input.txt', 'r') as f:
     lines = f.read().splitlines()
 
-lines = map(lambda line: line.split(': ')[1], lines)
+lines = map(lambda l: l.split(': ')[1], lines)
 
 total = 0
 
 card_copies = dict()
 for index, line in enumerate(lines):
-    winning, my = line.split(' | ')
-    
-    winning = re.split(r'\s+', winning)
-    my = re.split(r'\s+', my)
-    winning = list(filter(lambda x: len(x), winning))
-    
-    matches = set(winning) & set(my)
-    matches = len(matches) - ('' in my and '' in winning)
-    card_copies[index] = card_copies.get(index, 0) + 1
+    winning, my_numbers = line.split(' | ')
+
+    # parse my number
+    my_numbers = re.split(r'\s+', my_numbers.strip())
+
+    # parse winning numbers
+    winning = list(filter(lambda x: len(x), re.split(r'\s+', winning.strip())))
+
+    matches = len(set(winning) & set(my_numbers))  # count matches
+    card_copies[index] = card_copies.get(index, 0) + 1  # add the card itself
     for i in range(index + 1, matches + index + 1):
-        card_copies[i] = card_copies.get(i, 0) + card_copies[index]
-    pprint(index)
-    pprint(card_copies)
-    # print(set(winning) & set(my))
-    if matches:
+        card_copies[i] = card_copies.get(i, 0) + card_copies[index]  # add copies to next cards
+    if matches:  # if there are matches, add 2^(matches - 1) to total
+        # 2^(matches - 1) - value of card doubled for each match
         total += 2 ** (matches - 1)
         
 
 print(total)
 
 print(sum(card_copies.values()))
-
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import day1.script
 import day2.script
 import day3.script
+import day4.script
