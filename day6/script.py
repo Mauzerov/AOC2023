10998a2b8eef4f2a73a4df6e5f58b5196eac28b5
Mauzerov <mauzer.official.1@gmail.com>
2023-12-06 12:58:21 +0100

    day6 - easy mathematical solution (quadratic) removed class usage

--- day6/script.py
+++ day6/script.py
@@ -6,37 +6,29 @@ import re
 print(__file__)
 
 
-@dataclass
-class Race:
-    duration: int
-    distance: int
+def number_of_winning_ways(duration: int, distance: int) -> int:
+    """
+    mathematical formula:
+        for n in 0..duration
+        n * (duration - n) > distance
+        n^2 - duration * n + distance < 0
+    solution range:
+        delta = duration^2 - 4 * distance
+        n1 = (duration - sqrt(delta)) / 2  # smaller solution (rounded down)
+        n2 = (duration + sqrt(delta)) / 2  # bigger solution (rounded up)
 
-    def winning_ways(self) -> int:
-        """
-        mathematical formula:
-            for n in 0..duration
-            n * (duration - n) > distance
-            n^2 - duration * n + distance < 0
-        solution range:
-            delta = duration^2 - 4 * distance
-            n1 = (duration - sqrt(delta)) / 2  # smaller solution (rounded down)
-            n2 = (duration + sqrt(delta)) / 2  # bigger solution (rounded up)
+        n in (min(n1, n2), max(n1, n2))  # real solution range
 
-            n in (min(n1, n2), max(n1, n2))  # real solution range
+        number of integer solutions between n1 and n2:
+            n1 - n2 - 1
+    """
+    delta = duration ** 2 - 4 * distance
 
-            number of integer solutions between n1 and n2:
-                n1 - n2 - 1
-        :return:
-        """
-        duration, distance = self.duration, self.distance
+    n1 = (duration - delta ** 0.5) / 2
+    n2 = (duration + delta ** 0.5) / 2
+    n1, n2 = math.floor(n1), math.ceil(n2)
 
-        delta = duration ** 2 - 4 * distance
-
-        n1 = (duration - delta ** 0.5) / 2
-        n2 = (duration + delta ** 0.5) / 2
-        n1, n2 = math.floor(n1), math.ceil(n2)
-
-        return n2 - n1 - 1
+    return n2 - n1 - 1
 
 
 with open('day6/input.txt', 'r') as f:
@@ -46,13 +38,13 @@ with open('day6/input.txt', 'r') as f:
         for line in lines
     ]
 
-winning_ways = [Race(int(t), int(d)).winning_ways()
+winning_ways = [number_of_winning_ways(int(t), int(d))
                 for t, d in zip(*map(lambda l: l.split(' ')[1:], lines))]
 
 print(
     functools.reduce(int.__mul__, winning_ways)
 )
 
-print(Race(*one_race).winning_ways())
+print(number_of_winning_ways(*one_race))
 
 
