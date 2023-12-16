561af15f243b1ec42df62f5f54e4c6437beb2b8f
Mauzerov <mauzer.official.1@gmail.com>
2023-12-16 19:58:40 +0100

    Queue is slower than a list

--- day16/script.py
+++ day16/script.py
@@ -11,33 +11,43 @@ with open('day16/input.txt') as f:
 global_visited = set()
 
 
+# def timed(f):
+#     from time import perf_counter
+#
+#     def wrapper(*args, **kwargs):
+#         start = perf_counter()
+#         result = f(*args, **kwargs)
+#         end = perf_counter()
+#         print(f'{f.__name__} took {end - start:.15f} seconds', result, args)
+#         return result
+#     return wrapper
+#
+#
+# @timed
 def move_beam(start: tuple[int, ...]) -> int:
     visited = set()
-    to_visit = Queue()
-    to_visit.put(start)
-
-    while not to_visit.empty():
-        beam = to_visit.get()
-
+    to_visit = list()
+    to_visit.append(start)
+    while to_visit:
+        beam = to_visit.pop()
         x, y, dx, dy = beam
         if x < 0 or y < 0 or x >= size or y >= size or beam in visited:
             continue
 
         visited.add(beam)
-
         if grid[y][x] == '.':
-            to_visit.put((x + dx, y + dy, dx, dy))
+            to_visit.append((x + dx, y + dy, dx, dy))
             continue
         elif grid[y][x] in '|-':
             if grid[y][x] == '|' and dx != 0:
-                to_visit.put((x, y + 1, 0, 1))
-                to_visit.put((x, y - 1, 0, -1))
+                to_visit.append((x, y + 1, 0, 1))
+                to_visit.append((x, y - 1, 0, -1))
                 continue
             elif grid[y][x] == '-' and dy != 0:
-                to_visit.put((x + 1, y, 1, 0))
-                to_visit.put((x - 1, y, -1, 0))
+                to_visit.append((x + 1, y, 1, 0))
+                to_visit.append((x - 1, y, -1, 0))
                 continue
-            to_visit.put((x + dx, y + dy, dx, dy))
+            to_visit.append((x + dx, y + dy, dx, dy))
             continue
         elif grid[y][x] == '/':
             dy, dx = -dx, -dy
@@ -45,7 +55,7 @@ def move_beam(start: tuple[int, ...]) -> int:
             dy, dx = dx, dy
         else:
             raise RuntimeError('Unreachable')
-        to_visit.put((x + dx, y + dy, dx, dy))
+        to_visit.append((x + dx, y + dy, dx, dy))
 
     unique_visited = set((x, y) for x, y, _, _ in visited)
     global_visited.update(unique_visited)
@@ -54,15 +64,14 @@ def move_beam(start: tuple[int, ...]) -> int:
 
 print(move_beam((0, 0, 1, 0)))
 maximum = 0
-
 for i in range(size):
     if (0, i) not in global_visited:
         maximum = max(maximum, move_beam((0, i, 1, 0)))
     if (i, 0) not in global_visited:
         maximum = max(maximum, move_beam((i, 0, 0, 1)))
     if (size - 1, i) not in global_visited:
-        maximum = max(maximum, move_beam((size - 1, 0, 0, -1)))
+        maximum = max(maximum, move_beam((size - 1, i, 0, -1)))
     if (i, size - 1) not in global_visited:
-        maximum = max(maximum, move_beam((0, size - 1, -1, 0)))
+        maximum = max(maximum, move_beam((i, size - 1, -1, 0)))
 
 print(maximum)
