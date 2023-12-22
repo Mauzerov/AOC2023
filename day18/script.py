c8fa6ff01bb2fdc1e6b69b14dc9fcd14da273add
Mauzerov <mauzer.official.1@gmail.com>
2023-12-22 17:17:13 +0100

    apparently shoelace formula exists, and can be used while calculating the area (refactor parsing)

--- day18/script.py
+++ day18/script.py
@@ -9,25 +9,8 @@ print(__file__)
 class Node:
     x: int
     y: int
-    next_dir: str = None
-
     next: 'Node' = field(repr=False, default=None)
 
-    @property
-    def position(self) -> tuple[int, int]:
-        return self.x, self.y
-
-
-DIRECTIONS = {
-    'U': (0, -1),
-    'D': (0, 1),
-    'L': (-1, 0),
-    'R': (1, 0),
-    '0': (1, 0),
-    '1': (0, 1),
-    '2': (-1, 0),
-    '3': (0, -1),
-}
 
 DigPlan = NamedTuple('DigPlan', [('direction', str), ('distance', str), ('color', str)])
 
@@ -36,25 +19,6 @@ with open('day18/input.txt') as f:
         DigPlan(*re.search(r'([LURD]) (\d+) \(#([a-f0-9]{6})\)', line).groups())
         for line in f.read().splitlines()
     ]
-
-
-def shoelace_formula(node: Node) -> float:
-    dx, dy = lowest_xy(node)
-    dx, dy = abs(dx) + 1, abs(dy) + 1
-    start = curr = node
-    area = 0
-    while start != curr.next:
-        # shoe lace formula
-        area += (curr.x + dx) * (curr.next.y + dy) - (curr.y + dy) * (curr.next.x + dx)
-        # add edge
-        area += abs(curr.x - curr.next.x) + abs(curr.y - curr.next.y)
-        curr = curr.next
-
-    # add last edge
-    area += (curr.x + dx) * (curr.next.y + dy) - (curr.y + dy) * (curr.next.x + dx)
-    area += abs(curr.x - curr.next.x) + abs(curr.y - curr.next.y)
-
-    return area // 2 + 1  # +1 for the last corner
 
 
 def lowest_xy(node: Node) -> tuple[int, int]:
@@ -67,13 +31,45 @@ def lowest_xy(node: Node) -> tuple[int, int]:
     return x, y
 
 
-def parse_part1(_plans: list[DigPlan]) -> Node:
+def shoelace_formula(node: Node) -> float:
+    dx, dy = lowest_xy(node)
+    dx, dy = abs(dx) + 1, abs(dy) + 1
+    start = curr = node
+    area = 0
+    while start != curr.next:
+        # shoelace formula
+        area += (curr.x + dx) * (curr.next.y + dy) - (curr.y + dy) * (curr.next.x + dx)
+        # add edge
+        area += abs(curr.x - curr.next.x) + abs(curr.y - curr.next.y)
+        curr = curr.next
+
+    # add last edge
+    area += (curr.x + dx) * (curr.next.y + dy) - (curr.y + dy) * (curr.next.x + dx)
+    area += abs(curr.x - curr.next.x) + abs(curr.y - curr.next.y)
+
+    return area // 2 + 1  # +1 for the last corner
+
+
+def part1_directions(plan: DigPlan) -> tuple[str, int]:
+    return plan.direction, int(plan.distance)
+
+
+def part2_directions(plan: DigPlan) -> tuple[str, int]:
+    *distance_hex, direction = plan.color
+    return direction, int(''.join(distance_hex), 16)
+
+
+def parse(_plans: list[DigPlan], directions: callable) -> Node:
     start = Node(0, 0)
     node = start
     for plan in _plans:
-        dx, dy = DIRECTIONS[plan.direction]
-        node.next_dir = plan.direction
-        distance = int(plan.distance)
+        direction, distance = directions(plan)
+        dx, dy = {
+            'U': (0, -1), '3': (0, -1),
+            'D': (0,  1), '1': (0,  1),
+            'L': (-1, 0), '2': (-1, 0),
+            'R': (1,  0), '0': (1,  0),
+        }[direction]
         node.next = Node(
             node.x + dx * distance,
             node.y + dy * distance
@@ -83,22 +79,5 @@ def parse_part1(_plans: list[DigPlan]) -> Node:
     return start
 
 
-def parse_part2(_plans: list[DigPlan]) -> Node:
-    start = Node(0, 0)
-    node = start
-    for plan in _plans:
-        *distance_hex, direction = plan.color
-        distance = int(''.join(distance_hex), 16)
-        dx, dy = DIRECTIONS[direction]
-        node.next_dir = direction
-        node.next = Node(
-            node.x + dx * distance,
-            node.y + dy * distance,
-        )
-        node = node.next
-    node.next = start
-    return start
-
-
-print(shoelace_formula(parse_part1(plans)))
-print(shoelace_formula(parse_part2(plans)))
+print(shoelace_formula(parse(plans, part1_directions)))
+print(shoelace_formula(parse(plans, part2_directions)))
