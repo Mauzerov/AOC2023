34946d77d4bd5b4727850a464f8f554223e9fe54
Mauzerov <mauzer.official.1@gmail.com>
2023-12-22 14:56:55 +0100

    apparently shoelace formula exists, and can be used while calculating the area

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# import day1.script
-# import day2.script
-# import day3.script
-# import day4.script
-# import day5.script
-# import day6.script
-# import day7.script
-# import day8.script
-# import day9.script
-# import day10.script
-# import day11.script
-# import day12.script
-# import day13.script
-# import day14.script
-# import day15.script
-# import day16.script
-# import day17.script
+import day1.script
+import day2.script
+import day3.script
+import day4.script
+import day5.script
+import day6.script
+import day7.script
+import day8.script
+import day9.script
+import day10.script
+import day11.script
+import day12.script
+import day13.script
+import day14.script
+import day15.script
+import day16.script
+import day17.script
 import day18.script
